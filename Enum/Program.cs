48f1179ec88cfa87b8e69c57f8a6e792b6e84acc
Eunice Ricalday <e.ricalday.info@gmail.com>
2022-04-13 17:05:21 -0500

    Enum Meses

---
+++ Enum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+enum meses
+{
+	Enero,
+	Febrero,
+	Marzo,
+	Abril,
+	Mayo,
+	Junio,
+	Julio,
+	Agosto,
+	Septiembre,
+	Octubre,
+	Noviembre,
+	Diciembre
+}
+
+public class Program
+{
+	public static int Main()
+	{
+		Console.WriteLine(meses.Enero);
+		Console.WriteLine((meses)1);
+		return 0;
+	}
+}
