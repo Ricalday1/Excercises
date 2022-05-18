e8894b46847425dec5567da45d18592e89f9d7c4
Eunice Ricalday <e.ricalday.info@gmail.com>
2022-05-18 04:46:42 -0500

    Filereader

--- marvel/Heroesvillanos/FileManager/FileReader.cs
+++ marvel/Heroesvillanos/FileManager/FileReader.cs
@@ -1,18 +1,10 @@
 namespace Heroesvillanos;
-
-public class Parser 
+public class FileReader 
 {
-    public Heroevillano Parse(string line)
+
+    public static List<string> ReadFile (String path)
     {
-        var heroevillano = new Heroevillano();
-        var values = line.Split(',');
-        heroevillano.Id = long.Parse(values[0]);
-        heroevillano.Name = values[1];
-       // heroevillano.Gender = (Gender)Enum.Parse(typeof(Genre), values[2]);
-      //  heroevillno.Alignment = (Alignment)Enum.Parse(typeof(Alige), values[3]);
-        heroevillano.Overview = values[4];
-       // movie.Popularity = long.Parse(values[5]);
-        //movie.ProductionCompanies = values[6].Split('-').ToList();
-        return heroevillano;
+        return File.ReadAllLines(path).ToList();
     }
+    
 }
